b7562ff530a451e5285081ae19b51aadf20bf23e
rxfe-a <28RafaelA539@gmail.com>
2025-10-12 12:45:58 -0700

    add github repo

---
+++ public/static/js/cosmetic.js
@@ -0,0 +1,26 @@
+const F_MP3= 'https://github.com/rxfe-a/ytmp3-remake';
+const F_MP4= 'https://github.com/rxfe-softworks/ytmp4-remake';
+
+document.addEventListener("DOMContentLoaded", function() {
+    const githubIcon = document.getElementById("github");
+    githubIcon.addEventListener("click", function(event) {
+        event.preventDefault();
+        fetch('/serverdat/')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch server data');
+                }
+                return response.json();
+            })
+            .then(data => {
+                const target = (data.TARGET_FORMAT || 'mp3').trim().toLowerCase();
+                const redirectUrl = target === 'mp4' ? F_MP4 : F_MP3;
+                window.location.href = redirectUrl;
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                window.location.href = F_MP3;
+                target_format = 'mp3';
+            });
+    });
+});
--- source/index.js
+++ source/index.js
@@ -1,14 +1,32 @@
 require('dotenv').config();
 
 const fastify = require('fastify')({ logger: true });
+const path = require('path');
+const fs = require('fs').promises;
+
 fastify.register(require('@fastify/static'), {
-  root: require('path').join(__dirname, '..', 'public'),
+  root: path.join(__dirname, '..', 'public'),
   prefix: '/', 
+});
+
+fastify.get('/serverdat/', async (request, reply) => {
+  try {
+    const packageJsonPath = path.join(__dirname, '..', 'package.json');
+    const packageData = JSON.parse(await fs.readFile(packageJsonPath, 'utf8'));
+    return {
+      TARGET_FORMAT: process.env.TARGET_FORMAT,
+      homepage: packageData.homepage,
+      version: packageData.version,
+    };
+  } catch (error) {
+    fastify.log.error(error);
+    return reply.status(500).send({ error: 'Internal Server Error' });
+  }
 });
 
 const start = async () => {
   try {
-    await fastify.listen({ port: process.env.PORT || 3000});
+    await fastify.listen({ port: process.env.PORT || 3000 });
     console.log(`Server running on http://localhost:${process.env.PORT || 3000}`);
   } catch (err) {
     fastify.log.error(err);
